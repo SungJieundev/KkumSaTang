69f5fd4155261f6aa26bd2c4249d887c18ced849
Sijiwong <109190828+Sijiwong@users.noreply.github.com>
2025-07-12 05:53:37 +0900

    HeroMerge

---
+++ KST_GameJam2025/Assets/01.Scripts/Jiwoong/Spawner2.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+
+public class Spawner2 : MonoBehaviour
+{
+    public GameObject[] prefabsToSpawn; // 4개 프리팹을 인스펙터에 넣기
+
+    public void SpawnObject(Vector2 position)
+    {
+        if (prefabsToSpawn.Length == 0)
+        {
+            Debug.LogWarning("프리팹 리스트가 비어있습니다!");
+            return;
+        }
+
+        int randomIndex = Random.Range(0, prefabsToSpawn.Length); // 0부터 5까지 랜덤
+        GameObject selectedPrefab = prefabsToSpawn[randomIndex];
+
+        GameObject obj = Instantiate(selectedPrefab, position, Quaternion.identity);
+        ObjectManager.Instance.RegisterObject(obj); // 선택사항
+    }
+}
+
